653962eeb69edcabcfc10acac5466fdefa11010e
Zain <zainelabidine.bensaleh@esprit.tn>
2018-03-21 21:20:07 +0100

    Some changed code with Intellij IDEA

--- src/services/EtablissementService.java
+++ src/services/EtablissementService.java
@@ -30,7 +30,7 @@ public class EtablissementService implements IServiceEtablissement{
         System.out.println("EtablissementService");
                     try {
                              ste=DataSource.getInstance().getCon().createStatement();
-                            System.out.println("Connection établie");
+                            System.out.println("Con établie");
                     }
                     catch (SQLException e) {
                         System.out.println(e.getMessage());
