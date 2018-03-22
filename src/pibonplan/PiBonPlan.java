da0670b543b5e050835b8d21bd98e78f36e2b3de
Zain <zainelabidine.bensaleh@esprit.tn>
2018-03-22 22:19:50 +0100

    CRUD+Select One works ...FINISHED CRUD+get Utilisateur+Etablissement

--- src/pibonplan/PiBonPlan.java
+++ src/pibonplan/PiBonPlan.java
@@ -24,14 +24,6 @@ public class PiBonPlan {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        UtilisateurService user_serv=new UtilisateurService();
-        Utilisateur us = new Utilisateur();
-        //ArrayList<Utilisateur> uss= new ArrayList<>();
-         //uss= (ArrayList<Utilisateur>) user_serv.selectAll();
-        //System.out.println(uss);
-
-        //etab_serv.ajouter(new Etablissement("pino","Rue","ariana","ville",""));
-
 
     }
     
