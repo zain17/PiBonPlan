822d761ec46ee747f6219f47921b22c3a4db3252
Zain <zainelabidine.bensaleh@esprit.tn>
2018-03-27 11:36:13 +0100

    Liste utilisateurs ready to take data with id:fx and attributs and controller

--- src/gui/ListUtilisateursController.java
+++ src/gui/ListUtilisateursController.java
@@ -1,7 +1,20 @@
 package gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 public class ListUtilisateursController {
-    public TableView tab;
+    
+    @FXML private TableView tabview_users;
+    @FXML private TableColumn tabcol_id;
+    @FXML private TableColumn tabcol_photo;
+    @FXML private TableColumn tabcol_lang;
+    @FXML private TableColumn tabcol_lat;
+    @FXML private TableColumn tabcol_idetab;
+    @FXML private TableColumn tabcol_nom;
+    @FXML private TableColumn tabcol_prenom;
+    @FXML private TableColumn tabcol_email;
+    @FXML private TableColumn tabcol_lastlogin;
+    @FXML private TableColumn tabcol_role;
 }
