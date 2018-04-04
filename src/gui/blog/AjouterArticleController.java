1f64d15751562b7c1f394b493bb653814d1cc8c8
AMRI Amine <amri.amine.1@esprit.tn>
2018-04-04 18:44:01 +0100

    to push

--- src/gui/blog/AjouterArticleController.java
+++ src/gui/blog/AjouterArticleController.java
@@ -9,12 +9,15 @@ import gui.Main;
 import gui.Routers.RoutingGestionProfil;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.regex.Pattern;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.HTMLEditor;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 /**
  * FXML Controller class
@@ -41,8 +44,11 @@ public class AjouterArticleController implements Initializable {
     @FXML
     private void enregistrerArticle(ActionEvent event) {
         String texte = texteArticle.getHtmlText();
-        System.out.println(texte);
+        Document doc = Jsoup.parseBodyFragment(texte);
+        
+        String htmlText = doc.body().html();
         RoutingGestionProfil rGP = new RoutingGestionProfil(this.app);
+        
         rGP.gotoProfile();
       
     }
