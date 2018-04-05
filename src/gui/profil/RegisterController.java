17dbdb3ec6c941e130714bb72f4f92475b6f1999
Zain <zainelabidine.bensaleh@esprit.tn>
2018-04-05 20:11:53 +0100

    Redirection from login to registration After Registration Controller finished

--- src/gui/profil/RegisterController.java
+++ src/gui/profil/RegisterController.java
@@ -1,5 +1,6 @@
 package gui.profil;
 
+import entites.Utilisateur;
 import gui.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -13,26 +14,23 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class RegisterController implements Initializable{
-
+    @FXML private CheckBox chb_roleClient;
+    @FXML private CheckBox chb_roleEtab;
     @FXML private Hyperlink link_login;
     @FXML private TextField txt_user;
     @FXML private TextField txt_prenom;
     @FXML private TextField txt_email;
-    @FXML private CheckBox chb_roleetab;
     @FXML private TextField txt_password;
-    @FXML private CheckBox chb_roleclient;
     @FXML private TextField txt_confirmpass;
     @FXML private Button btn_enregistrer;
     @FXML private Button btn_annuler;
     private Main app;
+    private Utilisateur userToSave;
 
-    public void processLogout(ActionEvent actionEvent) {
-
-    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        userToSave=new Utilisateur();
     }
 
     public Main getApp() {
@@ -41,5 +39,22 @@ public class RegisterController implements Initializable{
 
     public void setApp(Main main) {
         this.app = main;
+    }
+
+    public void onClickEnregistrer(ActionEvent actionEvent) {
+        if (dataValidation()) {
+            userToSave.setUsername(txt_prenom.getText());
+            userToSave.setUsernameCanonical(txt_user.getText());
+            userToSave.setEmail(txt_email.getText());
+//            userToSave.set
+        }
+    }
+
+    public void onAnnuller(ActionEvent actionEvent) {
+
+    }
+    public boolean dataValidation(){
+        // TODO: Warning - Cette méthode valide tous les donnés taper dans le formulaire (aucun passage sauf qu'elle return true)
+        return true;
     }
 }
