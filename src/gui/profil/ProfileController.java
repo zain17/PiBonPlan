a14125413613d64899838189bc60bfc80960e670
Zain <zainelabidine.bensaleh@esprit.tn>
2018-04-07 22:49:31 +0100

    loadInfofrom database to profile

--- src/gui/profil/ProfileController.java
+++ src/gui/profil/ProfileController.java
@@ -5,13 +5,24 @@ import gui.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import services.UtilisateurService;
 
+import java.net.Authenticator;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ProfileController implements Initializable {
+    @FXML
+    private  Label lbl_nbExperience;
+    @FXML
+    private  Label lbl_nbRevue;
+    @FXML
+    private  Label lbl_nbEvents;
+    @FXML
+    private  Hyperlink H_gotoMyEtab;
     @FXML
     private Label lbl_nometablissement;
     @FXML
@@ -37,5 +48,17 @@ public class ProfileController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("initialize prompt");
+        loadUserInfo(security.Authenticator.getCurrentAuth());
     }
+    public void loadUserInfo(Utilisateur u){
+        UtilisateurService usev=new UtilisateurService();
+        lbl_nom.setText(u.getUsername());
+        lbl_prenom.setText(u.getUsername());
+        lbl_email.setText(u.getEmail());
+        lbl_nbEvents.setText(String.valueOf(usev.nbEvents(u.getId())));
+        lbl_nbExperience.setText(String.valueOf(usev.nbExperiences(u.getId())));
+        lbl_nbRevue.setText(String.valueOf(usev.nbRevues(u.getId())));
+    }
+
+
 }
