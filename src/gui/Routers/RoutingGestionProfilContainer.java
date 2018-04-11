e8613c42c29b83b8195d8a1ec5aa99aa13194292
Zain <zainelabidine.bensaleh@esprit.tn>
2018-04-11 15:44:44 +0100

    Add Constructeur dans Routing GestionProfilContainer

--- src/gui/Routers/RoutingGestionProfilContainer.java
+++ src/gui/Routers/RoutingGestionProfilContainer.java
@@ -3,12 +3,10 @@ package gui.Routers;
 import gui.*;
 import gui.profil.*;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 public class RoutingGestionProfilContainer {
     private Main main;
@@ -16,6 +14,12 @@ public class RoutingGestionProfilContainer {
     private FormUtilisateur currentFormUtilisateur;
     private FormEtablissementController currentFormEtablissement;
     private FormEtablissementEditController currentformEtablissementEditController;
+    private ListetablissementController curreListEtab;
+
+    public RoutingGestionProfilContainer(Main main, ListetablissementController curreListEtab) {
+        this.main = main;
+        this.curreListEtab = curreListEtab;
+    }
 
     public RoutingGestionProfilContainer(Main main, ProfileController currentProfileController) {
         this.main = main;
@@ -37,11 +41,9 @@ public class RoutingGestionProfilContainer {
         this.currentformEtablissementEditController = formEtablissementEditController;
     }
 
-
     public void returnFromEdit() throws IOException {
             currentFormUtilisateur.getConteneurProfil().getChildren().setAll((AnchorPane)FXMLLoader.load(getClass().getResource("/gui/profil/profile.fxml")));
     }
-
     public void profileToEtablissement() throws IOException {
         currentProfileController.getContneurProfil().getChildren().setAll((AnchorPane)FXMLLoader.load(getClass().getResource("/gui/profil/formEtablissement.fxml")));
     }
@@ -55,5 +57,4 @@ public class RoutingGestionProfilContainer {
     public void returnFromEtabVersProfile() throws IOException {
         currentFormEtablissement.getContainer_formEtab().getChildren().setAll((AnchorPane)FXMLLoader.load(getClass().getResource("/gui/profil/profile.fxml")));
     }
-
 }
