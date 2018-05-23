271aa6297670f4120a75afd9c21a4f3bead43c4e
amineamri <amine.amri.1@esprit.tn>
2018-05-23 19:04:31 +0100

    basic data validation

--- src/gui/blog/AjouterArticleController.java
+++ src/gui/blog/AjouterArticleController.java
@@ -25,9 +25,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
 import javafx.scene.web.HTMLEditor;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -40,10 +43,9 @@ import services.TagService;
  *
  * @author aminos
  */
-
 public class AjouterArticleController implements Initializable {
 
-     public Main app;
+    public Main app;
     @FXML
     private Button sauvegarderArticle;
     @FXML
@@ -62,7 +64,7 @@ public class AjouterArticleController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 
     @FXML
     private void enregistrerArticle(ActionEvent event) throws IOException {
@@ -70,37 +72,41 @@ public class AjouterArticleController implements Initializable {
         TagService tS = new TagService();
         String texte = texteArticle.getHtmlText();
         Document doc = Jsoup.parseBodyFragment(texte);
-        
+
         String htmlText = doc.body().html();
         String tagsStr = tagsTexte.getText();
         String titre = titreArticle.getText();
-        ArrayList<Tag> tagsArray = null;
-        if (tS.isValidTags(tagsStr)) {
-            System.out.println("TAgj" + tS.tagsStringToTags(tagsStr).size());
-            tagsArray = tS.tagsStringToTags(tagsStr);
-        }
-        //System.out.println("Debug AjoutA***" + (this.app != null));
-                 //   Logger.getagsStrtLogger(AjouterArticleController.class.getName()).log(Level.SEVERE, null, this.app != null);
-
-        Article a = new Article(htmlText, 0, titre, 3, Authenticator.getCurrentAuth().getUsername(), new Date(), new Date());
-        aS.ajouter(a);
-        if (tagsArray != null && ! tagsArray.isEmpty()) {
-            for (Tag t : tagsArray) {
-                tS.ajouter(t, a.getId());
-                System.out.println("Tag: " + t.getName());
+        if (htmlText.length() != 0 && titre.length() != 0) {
+            ArrayList<Tag> tagsArray = null;
+            if (tS.isValidTags(tagsStr)) {
+                System.out.println("TAgj" + tS.tagsStringToTags(tagsStr).size());
+                tagsArray = tS.tagsStringToTags(tagsStr);
             }
+            //System.out.println("Debug AjoutA***" + (this.app != null));
+            //   Logger.getagsStrtLogger(AjouterArticleController.class.getName()).log(Level.SEVERE, null, this.app != null);
+
+            Article a = new Article(htmlText, 0, titre, 3, Authenticator.getCurrentAuth().getUsername(), new Date(), new Date());
+            aS.ajouter(a);
+            if (tagsArray != null && !tagsArray.isEmpty()) {
+                for (Tag t : tagsArray) {
+                    tS.ajouter(t, a.getId());
+                    System.out.println("Tag: " + t.getName());
+                }
+            }
+
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/gui/blog/lireArticle.fxml"));
+            AnchorPane forInserting = (AnchorPane) fxmlLoader.load();
+            LireArticleController c = (LireArticleController) fxmlLoader.getController();
+            System.out.println("AjouterArticle" + c.blogController != null);
+            c.blogController = blogController;
+            c.setArticle(a);
+            blogWidget.getChildren().setAll(forInserting);
+
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Titre ou contenu de l'article est vide!", ButtonType.OK);
+            alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+            alert.show();
         }
-        
-         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/gui/blog/lireArticle.fxml"));
-        AnchorPane forInserting  = (AnchorPane) fxmlLoader.load();
-        LireArticleController c = (LireArticleController) fxmlLoader.getController();
-        System.out.println("AjouterArticle" + c.blogController != null);
-        c.blogController = blogController;
-        c.setArticle(a);
-        blogWidget.getChildren().setAll(forInserting);
-       
-        
-      
     }
 
     public void setApp(Main app) {
